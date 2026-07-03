3959eb2b61c05bee6de456105babd81e21426163
techbuuren-ops <techbuuren@gmail.com>
2026-07-03 13:05:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
 )
 
 # Cadena de conexión oficial a tu Supabase
-DB_URI = "postgresql://postgres.sohavacqphahseoezale:[Buuren2708a]@aws-1-us-west-2.pooler.supabase.com:5432/postgres"
+DB_URI = "postgresql://postgres.sohavacqphahseoezale:[Buuren2708@]@aws-1-us-west-2.pooler.supabase.com:5432/postgres"
 CARPETA_UPLOADS = "uploads"
 
 if not os.path.exists(CARPETA_UPLOADS):
